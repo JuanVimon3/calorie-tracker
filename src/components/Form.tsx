aa832a52585e9466585be17ee74e2e398bd236b3
JuanVimon3 <jvillamonsalve@gmail.com>
2026-01-13 16:01:30 -0500

    WIP

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,13 @@
 import { categories } from "../data/categories";
 import { v4 as uuidv4 } from "uuid";
-import type { ActivityActions } from "../reducers/activity-reducer";
+import type { ActivityActions, ActivityState } from "../reducers/activity-reducer";
 import type { Activity } from "../types";
-import { useState, type ChangeEvent, type FormEvent, type Dispatch } from "react"
+import { useState, type ChangeEvent, type FormEvent, type Dispatch, useEffect } from "react"
 
 
  type FormProps = {
-    dispatch: Dispatch<ActivityActions>
+    dispatch: Dispatch<ActivityActions>,
+    state: ActivityState
   }
 
 
@@ -16,9 +17,16 @@ import { useState, type ChangeEvent, type FormEvent, type Dispatch } from "react
     name: '',
     calories: 0
   }
-export default function Form({dispatch} : FormProps) {
+export default function Form({dispatch, state} : FormProps) {
 
   const [activity, setActivity] = useState<Activity>(initialState)
+
+  useEffect(() => {
+    if(state.activeId){
+      const selectedActivity = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
+      setActivity(selectedActivity)
+    }
+  }, [state.activeId])
 
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
